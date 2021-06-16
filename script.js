2add82d8a86119e6c91479d6d7446ac1074621ec
gianluca galota <gianluca.galota@gmail.com>
2021-06-16 22:34:05 +0100

    highscore funcionality implemented

--- script.js
+++ script.js
@@ -6,38 +6,54 @@ const guessInput = document.querySelector('.guess');
 const message = document.querySelector('.message');
 const randNum = document.querySelector('.number');
 const btnRefresh = document.querySelector('.again');
+const body = document.querySelector('body');
 
+// Selectin dynamic element
 let score = document.querySelector('.score');
 let highScore = document.querySelector('.highscore');
 
+let firstTime = false;
+
+// Deactive check button
 btnCheck.disabled = false;
 
 // Generate Random Number between 1 and 20
 let genRandNum = Math.floor(Math.random() * 20) + 1;
-randNum.textContent = genRandNum;
+
+// Generate message game
+const displayGameMsg = (msg) => {
+  message.textContent = msg
+}
+
+// Change body background color
+const changeBgColor = () => {
+  body.style.backgroundColor = Number(guessInput.value) === genRandNum ? '#60b347' : '#222';
+}
 
 // Creating functions
 const checkGuessInput = (guess) => {
+  // Turn string into Integer
   guess = Number(guess.value);
+  // When guess is not a number
   if (!guess) {
-    message.textContent = "⛔️ this is not a valid number";
-  } else if (guess > genRandNum) {
-    message.textContent = "📈 number too high";
-    score.innerText -= 1;
-    if (score.innerText < 1) {
-      message.textContent = "💥 Sorry you lost the game";
+    displayGameMsg("⛔️ this is not a valid number");
+    // When guess is wrong
+  } else if (guess !== genRandNum) {
+    if (guess >= 1) {
+      displayGameMsg(guess > genRandNum ? "📈 number too high" : "📉 number too low");
+      score.innerText -= 1;
+    } else {
+      displayGameMsg("💥 Sorry you lost the game");
+      score.innerText = 0;
       btnCheck.disabled = true;
     }
-  } else if (guess < genRandNum) {
-    message.textContent = "📉 number too low";
-    score.innerText -= 1;
-    if (score.innerText < 1) {
-      message.textContent = "💥 Sorry you lost the game";
-      btnCheck.disabled = true;
-    }
+    // When guess is correct  
   } else if (guess === genRandNum) {
-    message.textContent = "🎉 Congratulations, you guessed the secret number";
-    // highScore.textContent += 1;
+    displayGameMsg("🎉 Congratulations, you guessed the secret number");
+    highScore.innerText = score.innerText;
+    changeBgColor();
+    randNum.textContent = genRandNum;
+    randNum.style.width = "30rem";
   }
 }
 
@@ -47,6 +63,13 @@ btnCheck.addEventListener('click', () => {
 })
 
 btnRefresh.addEventListener('click', () => {
-  window.location.reload();
+  genRandNum;
+  highScore.textContent = score.innerText;
+  randNum.style.width = "15rem";
+  randNum.textContent = '?';
+  guessInput.value = '';
+  score.innerText = '20';
+  checkGuessInput(guessInput);
+  changeBgColor();
+  displayGameMsg("Start guessing...");
 })
-
